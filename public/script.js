425d4ce23c0ffd9ad50881ab305fc56c9b7cbe90
AYehia0 <ahmedyehia611@gmail.com>
2021-07-07 18:52:49 +0200

    calling the internal api /getinfo, and create an image

--- public/script.js
+++ public/script.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+function createImage(imgUrl){
+    //check if the img exists
+    imgExist = document.getElementById("dinoImg")
+
+    if (imgExist) {
+        imgExist.remove()
+    }
+
+    //create img element
+    var img = document.createElement("img")
+    img.src = imgUrl
+    img.id = "dinoImg"
+    document.body.appendChild(img) 
+
+}
+
+
+async function getInfo() {
+    //calls the api to fetch the data 
+    const res = await fetch("/getinfo")
+    const data = await res.json()
+
+    dinoName = data["name"]
+    dinoImg = data["url"]
+    // show in the HTML
+    document.getElementById("dinoName").textContent = dinoName 
+    createImage(dinoImg)
+
+}
